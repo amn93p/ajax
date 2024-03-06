f4f450ae7c8665b5af184d4a1e058609d5be6fa3
amn93p <abbenfarhat@gmail.com>
2024-03-06 16:42:37 +0100

    derniere mise à jour

--- login.php
+++ login.php
@@ -4,4 +4,8 @@ if (!empty($_POST["login"]) && !empty($_POST["password"])){
     echo "L'identifiant tapé est ". $_POST["login"]. "<br>";
     echo "Le mot de passe tapé est ". $_POST["password"]. "<br>";
 }
+else{
+    echo "Veuillez entrer les informations requises. ";
+
+}
 ?>
